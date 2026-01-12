32ac7e28ad2ca8ac6a21e5c9c3bebe48e00c76fa
Ardhika Putra <ardhikaptr11@gmail.com>
2026-01-12 20:15:04 +0700

    build(config): enable react compiler and configure experimental features

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactCompiler: true,
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+    serverActions: {
+      bodySizeLimit: "2mb"
+    }
+  },
+  images: {
+    remotePatterns: [
+      new URL(`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/**`),
+    ]
+  },
 };
 
 export default nextConfig;
