495a5cb21ebcf4b7dafb38c2cedf07ea43567e4a
Ardhika Putra <ardhikaptr11@gmail.com>
2026-02-23 01:06:06 +0700

    refactor(loader): simplify screen loader state management and rendering

--- app/(root)/components/screen-loader.tsx
+++ app/(root)/components/screen-loader.tsx
@@ -2,7 +2,6 @@
 
 import { Fragment, ReactNode, useEffect, useState } from "react";
 import Loader from "./loader";
-import { cn } from "@/lib/utils";
 
 const ScreenLoader = ({
   children,
@@ -13,69 +12,39 @@ const ScreenLoader = ({
 }) => {
   const [isCounterDone, setCounterDone] = useState(false);
   const [animateOut, setAnimateOut] = useState(false);
-  const [isLoaded, setIsLoaded] = useState<boolean>(false);
-  const [shouldRenderLoader, setShouldRenderLoader] = useState(true);
+  const [isLoaded, setIsLoaded] = useState<boolean | null>(null);
 
   useEffect(() => {
-    document.body.style.overflow = isLoaded ? "unset" : "hidden";
-
-    return () => {
-      document.body.style.overflow = "unset";
-    };
-  }, [isLoaded]);
-
-  useEffect(() => {
-    const finished = sessionStorage.getItem("isLoaderFinished") === "true";
-    if (finished) {
-      setIsLoaded(true);
-      setShouldRenderLoader(false);
-    }
+    const isLoaderFinished = sessionStorage.getItem("isLoaderFinished");
+    setIsLoaded(isLoaderFinished === "true");
   }, []);
 
   useEffect(() => {
     if (isCounterDone) {
-      const timeout = setTimeout(() => {
-        setAnimateOut(true);
-        setIsLoaded(true);
-      }, 800);
-
+      const timeout = setTimeout(() => setAnimateOut(true), 800);
       return () => clearTimeout(timeout);
     }
   }, [isCounterDone]);
 
   const handleFinish = () => {
     sessionStorage.setItem("isLoaderFinished", "true");
-    setTimeout(() => setShouldRenderLoader(false), 1000);
+    setIsLoaded(true);
   };
 
-  return (
-    <Fragment>
-      {shouldRenderLoader && (
-        <div
-          className={cn(
-            "fixed inset-0 z-9999 transition-transform duration-1000 ease-in-out",
-            animateOut ? "-translate-y-full" : "translate-y-0",
-          )}
-        >
-          <Loader
-            taglines={taglines}
-            animateOut={animateOut}
-            onCounterDone={() => setCounterDone(true)}
-            onFinish={handleFinish}
-          />
-        </div>
-      )}
+  if (isLoaded === null) return null;
 
-      <div
-        className={cn(
-          "transition-opacity duration-1000 ease-in-out",
-          !isLoaded ? "opacity-0" : "opacity-100",
-        )}
-      >
-        {children}
-      </div>
-    </Fragment>
-  );
+  if (!isLoaded) {
+    return (
+      <Loader
+        taglines={taglines}
+        animateOut={animateOut}
+        onCounterDone={() => setCounterDone(true)}
+        onFinish={handleFinish}
+      />
+    );
+  }
+
+  return <Fragment>{children}</Fragment>;
 };
 
 export default ScreenLoader;
